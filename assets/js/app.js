cc4d7f809e93384e05b97e41f6e09492c26b2996
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-05-29 23:05:33 -0300

    Add bolinha draw

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,13 +18,59 @@ import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let Hooks = {}
 
-// Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+Hooks.canvas = {
+  mounted() {
+    let canvas = this.el;
+    let context = canvas.getContext("2d");
 
-// connect if there are any LiveViews on the page
+    window.addEventListener("mousedown", (e) => {
+      this.pushEvent("mousedown", { x: e.clientX, y: e.clientY })
+    })
+
+    Object.assign(this, { canvas, context });
+  },
+  updated() {
+    let { canvas, context } = this;
+
+    let halfHeight = canvas.height / 2;
+    let halfWidth = canvas.width / 2;
+    let smallerHalf = Math.min(halfHeight, halfWidth);
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "rgba(128, 0, 255, 1)";
+    context.beginPath();
+    context.arc(
+      this.canvas.dataset.drawx,
+      this.canvas.dataset.drawy,
+      smallerHalf / 16,
+      0,
+      2 * Math.PI
+      );
+      context.fill();
+    }
+  };
+
+  // Show progress bar on live navigation and form submits
+  window.addEventListener("phx:page-loading-start", info => NProgress.start())
+  window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+
+  // connect if there are any LiveViews on the page
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  params: {
+    _csrf_token: csrfToken
+  },
+  metadata: {
+    click: (e, el) => {
+      return {
+        clientX: e.clientX,
+        clientY: e.clientY
+      }
+    }
+  }
+})
 liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
--- lib/pente_web/live/page_live.ex
+++ lib/pente_web/live/page_live.ex
@@ -3,37 +3,43 @@ defmodule PenteWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    Process.send_after(self(), :joao, 100)
+
+    socket = socket
+      |> assign(:last_draw, %{x: nil, y: nil})
+      |> assign(:mimi, 0)
+
+    {:ok, socket}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_info(:joao, %{assigns: %{mimi: mimi}} = socket) do
+    # Process.send_after(self(), :joao, 100)
+
+    {:noreply, assign(socket, :mimi, mimi + 1)}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  def handle_event("cris", event, socket) do
+    IO.inspect(event, label: :event)
 
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+    {:noreply, socket}
   end
 
-  defp search(query) do
-    if not PenteWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  @impl true
+  def handle_event("mousedown", event, socket) do
+    IO.inspect(event, label: :event)
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    {:noreply, assign(socket, :last_draw, event)}
+  end
+
+  @impl true
+  def render(%{last_draw: last_draw } = assigns) do
+    x = last_draw["x"]
+    y = last_draw["y"]
+
+    ~L"""
+      <canvas data-mimi="<%= @mimi %>" data-drawX="<%= x %>" data-drawY="<%= y %>" phx-hook="canvas" phx-click="cris" id="marcos" width="1000" height="1000"></canvas>
+    """
   end
 end
